b180eaac3f63903a3a7c9a5acd79acdc2286415e
Taiju1192 <netaka1192@gmail.com>
2025-07-22 23:59:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3939;
 app.listen(PORT, () => {
   console.log(`🌐 Webサーバー起動中: http://localhost:${PORT}`);
 });
