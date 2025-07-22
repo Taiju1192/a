38b3d51cf7b17917f8ab7687e87aae890ce15435
Taiju1192 <netaka1192@gmail.com>
2025-07-22 20:09:03 +0900

    Update tracks.js

--- data/tracks.js
+++ data/tracks.js
@@ -59,22 +59,7 @@ const tracks = [
     title: "友成空(TOMONARI SORA) - ”睨めっ娘”",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E5%8F%8B%E6%88%90%E7%A9%BA(TOMONARI%20SORA)%20-%20%E2%80%9DNiramekko%E2%80%9D%20%EF%BC%BBLyric%20Video%EF%BC%BD.mp4?v=1750427422402"
   }, 
-  {
-    title: "米津玄師 - 感電",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%AB%20-%20%E6%84%9F%E9%9B%BB%20%20%20Kenshi%20Yonezu%20-%20KANDEN.mp4?v=1750427422402"
-  }, 
-  {
-    title: "米津玄師 - KICKBACK",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%AB%20Kenshi%20Yonezu%20-%20KICKBACK.mp4?v=1750427426374"
-  }, 
-  {
-    title: "米津玄師 - 馬と鹿",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%AB%20%20-%20%E9%A6%AC%E3%81%A8%E9%B9%BF%20%20%20Kenshi%20Yonezu%20-%20Uma%20to%20Shika.mp4?v=1750427429144"
-  }, 
-  {
-    title: "米津玄師 - Flamingo",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%AB%20%20-%20Flamingo%20%EF%BC%8F%20Kenshi%20Yonezu.mp4?v=1750427433590"
-  }, 
+
   {
     title: "川崎鷹也-魔法の絨毯",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E5%B7%9D%E5%B4%8E%E9%B7%B9%E4%B9%9F-%E9%AD%94%E6%B3%95%E3%81%AE%E7%B5%A8%E6%AF%AF%E3%80%90OFFICIAL%20MUSIC%20VIDEO%E3%80%91.mp4?v=1750427440415"
@@ -84,21 +69,13 @@ const tracks = [
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%AB%20%20-%20%E3%82%A2%E3%82%A4%E3%83%8D%E3%82%AF%E3%83%A9%E3%82%A4%E3%83%8D%20%2C%20Kenshi%20Yonezu%20-%20Eine%20Kleine.mp4?v=1750427440457"
   }, 
   {
-    title: "米津玄師 - ピースサイン",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E7%B1%B3%E6%B4%A5%E7%8E%84%E5%B8%AB%20-%20%E3%83%92%E3%82%9A%E3%83%BC%E3%82%B9%E3%82%B5%E3%82%A4%E3%83%B3%20%2C%20Kenshi%20Yonezu%20-%20Peace%20Sign.mp4?v=1750427447825"
-  }, 
-  {
     title: "怪獣の花唄 ／ Vaundy",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E6%80%AA%E7%8D%A3%E3%81%AE%E8%8A%B1%E5%94%84%20%EF%BC%8F%20Vaundy%20%EF%BC%9A%20MUSIC%20VIDEO.mp4?v=1750427449224"
   }, 
   {
     title: "はいよろこんで ／ こっちのけんと",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E3%81%AF%E3%81%84%E3%82%88%E3%82%8D%E3%81%93%E3%82%93%E3%81%A6%E3%82%99%20%EF%BC%8F%20%E3%81%93%E3%81%A3%E3%81%A1%E3%81%AE%E3%81%91%E3%82%93%E3%81%A8%20MV.mp4?v=1750427465064"
-  }, 
-  {
-    title: "菅田将暉 『まちがいさがし』",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E8%8F%85%E7%94%B0%E5%B0%86%E6%9A%89%20%E3%80%8E%E3%81%BE%E3%81%A1%E3%81%8B%E3%82%99%E3%81%84%E3%81%95%E3%81%8B%E3%82%99%E3%81%97%E3%80%8F.mp4?v=1750427468587"
-  }, 
+  },
   {
     title: "マカロニえんぴつ「なんでもないよ、」",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E3%83%9E%E3%82%AB%E3%83%AD%E3%83%8B%E3%81%88%E3%82%93%E3%81%B2%E3%82%9A%E3%81%A4%E3%80%8C%E3%81%AA%E3%82%93%E3%81%A6%E3%82%99%E3%82%82%E3%81%AA%E3%81%84%E3%82%88%E3%80%81%E3%80%8DMV.mp4?v=1750427469638"
@@ -138,10 +115,6 @@ const tracks = [
   {
     title: "Official髭男dism - I LOVE...",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/Official%E9%AB%AD%E7%94%B7dism%20-%20I%20LOVE...%EF%BC%BBOfficial%20Video%EF%BC%BD.mp4?v=1750427536520"
-  },
-  {
-    title: "nobodyknows+「ココロオドル」",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/nobodyknows%2B%E3%80%8C%E3%82%B3%E3%82%B3%E3%83%AD%E3%82%AA%E3%83%88%E3%82%99%E3%83%AB%E3%80%8DOFFICIAL%20MUSIC%20VIDEO.mp4?v=1750427543924"
   },
   {
     title: "MY FIRST STORY - I'm a mess",
@@ -200,10 +173,6 @@ const tracks = [
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/DISH%EF%BC%8F%EF%BC%8F%20-%20%E7%8C%AB%20%5BOfficial%20Live%20Video%202019%5D.mp4?v=1750427606488"
   },
   {
-    title: "GReeeeN - キセキ",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/GReeeeN%20-%20%E3%82%AD%E3%82%BB%E3%82%AD.mp4?v=1750427614450"
-  },
-  {
     title: "King Gnu - TWILIGHT!!!",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/King%20Gnu%20-%20TWILIGHT!!!.mp4?v=1750427617021"
   },
@@ -235,10 +204,6 @@ const tracks = [
     title: "「前前前世」",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/RADWIMPS%20-%20%E5%89%8D%E5%89%8D%E5%89%8D%E4%B8%96%20(movie%20ver.)%20%5BOfficial%20Music%20Video%5D.mp4?v=1750911750416"
   },
-    {
-    title: "ビリミリオン",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E5%84%AA%E9%87%8C%E3%80%8E%E3%83%92%E3%82%99%E3%83%AA%E3%83%9F%E3%83%AA%E3%82%AA%E3%83%B3%E3%80%8FOfficial%20Music%20Video.mp4?v=1750940890648" // ←リンクを後で追加してください
-  },
   {
     title: "１１５万キロのフィルム",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/Official%E9%AB%AD%E7%94%B7dism%20-%20115%E4%B8%87%E3%82%AD%E3%83%AD%E3%81%AE%E3%83%95%E3%82%A3%E3%83%AB%E3%83%A0%EF%BC%BBOfficial%20Audio%EF%BC%BD.mp4?v=1750940873834"
@@ -260,10 +225,6 @@ const tracks = [
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E8%8F%85%E7%94%B0%E5%B0%86%E6%9A%89%20%E3%80%8E%E3%81%95%E3%82%88%E3%81%AA%E3%82%89%E3%82%A8%E3%83%AC%E3%82%B7%E3%82%99%E3%83%BC%E3%80%8F.mp4?v=1750940939916"
   },
   {
-    title: "白日",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/King%20Gnu%20-%20%E7%99%BD%E6%97%A5.mp4?v=1750940958249"
-  },
-  {
     title: "ノーダウト",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/Official%E9%AB%AD%E7%94%B7dism%20-%20%E3%83%8E%E3%83%BC%E3%82%BF%E3%82%99%E3%82%A6%E3%83%88%EF%BC%BBOfficial%20Video%EF%BC%BD.mp4?v=1750940978797"
   },
@@ -274,14 +235,6 @@ const tracks = [
   {
     title: "スターマイン",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/Da-iCE%20%EF%BC%8F%20%E3%80%8C%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%9E%E3%82%A4%E3%83%B3%E3%80%8DMusic%20Video.mp4?v=1750940930593"
-  },
-  { 
-    title: "裸の心",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E3%81%82%E3%81%84%E3%81%BF%E3%82%87%E3%82%93%20-%20%E8%A3%B8%E3%81%AE%E5%BF%83%E3%80%90OFFICIAL%20MUSIC%20VIDEO%E3%80%91.mp4?v=1750940992516"
-  },
-  {
-    title: "相思相愛",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E7%9B%B8%E6%80%9D%E7%9B%B8%E6%84%9B.mp4?v=1750940940155"
   },
   {
     title: "ヨワネハキ",
@@ -294,10 +247,6 @@ const tracks = [
   {
     title: "ただ君に晴れ",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E3%83%A8%E3%83%AB%E3%82%B7%E3%82%AB%20-%20%E3%81%9F%E3%81%9F%E3%82%99%E5%90%9B%E3%81%AB%E6%99%B4%E3%82%8C%20(MUSIC%20VIDEO).mp4?v=1750940912372"
-  },
-  {
-    title: "友よ～この先もずっと・・・",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E5%8F%8B%E3%82%88%20%EF%BD%9E%20%E3%81%93%E3%81%AE%E5%85%88%E3%82%82%E3%81%99%E3%82%99%E3%81%A3%E3%81%A8%EF%BD%A5%EF%BD%A5%EF%BD%A5.mp4?v=1750941341427"
   },
   {
     title: "虹色の戦争",
@@ -314,18 +263,6 @@ const tracks = [
   {
     title: "overdose",
     src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E3%81%AA%E3%81%A8%E3%82%8A%20-%20Overdose.mp4?v=1750940893541"
-  },
-  {
-    title: "虹",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E8%99%B9%20-%20%E8%8F%85%E7%94%B0%E5%B0%86%E6%9A%89%EF%BC%88%E3%83%95%E3%83%AB%EF%BC%89.mp4?v=1750940920975"
-  },
-  {
-    title: "アイノカタチ",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/MISIA%20-%20%E3%82%A2%E3%82%A4%E3%83%8E%E3%82%AB%E3%82%BF%E3%83%81%20feat.HIDE(GReeeeN)%20%EF%BC%88Official%20Music%20Video%EF%BC%89.mp4?v=1750940887757"
-  },
-  {
-    title: "香水",
-    src: "https://cdn.glitch.global/1a7b0d9f-053a-4c34-817e-4c9c9da5b28a/%E9%A6%99%E6%B0%B4%20%20%EF%BC%8F%20%20%E7%91%9B%E4%BA%BA%20(Official%20Music%20Video).mp4?v=1750940926267"
   },
   // ...全曲分続けて追加...
 ];
